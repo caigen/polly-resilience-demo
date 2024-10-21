48c285a5fbb71b5752cd16e22170eaac8a4be702
Gen Cai <gencai@microsoft.com>
2024-10-21 11:32:14 +0800

    Resilience Controller

---
+++ ClientSide/Controllers/ResilienceController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ClientSide.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResilienceController : ControllerBase
+    {
+        // GET: api/<ResilienceController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ResilienceController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ResilienceController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ResilienceController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ResilienceController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
